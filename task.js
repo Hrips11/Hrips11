0c3fbf15a10424acb7bf745ee715be6fbbdb9f10
Hripsime <sanbaxd@mail.ru>
2022-04-14 20:10:39 +0400

    Js Game

--- task.js
+++ task.js
@@ -1,20 +1,22 @@
+let name = prompt("Please enter your name");
+
 let qa = ["0 - Tatevik", "    1 -  Hripsime", "    2 - Anna", "    3 - Vazgen", "    4 - Arpine", "    5 - Karine", "     6 - Sergey", "    7 - Ani", "     8 - Tigran", "     9 - Styopa", "       10 - Narek", "    11 - Narine"];
 
 while (true) {
 
-  let answer = prompt("Who is your favoite QA engineer? \n\n" + qa + "\n\n" + "Please write the number." + "\n\n" + "If you want to exit press Exit");
+  let answer = prompt(`Hi ${name}\nWho is your favoite QA engineer? \n\n${qa}\n\nPlease write the number.\n\nIf you want to exit press Exit.`);
 
 
-  if (answer == "exit") {
-    alert("Thank you");
+  if (answer === "Exit") {
+    alert(`Thank you ${name}`);
     break;
   }
   let answer1 = +answer;
   if (qa.includes(qa[answer1])) {
     alert("Good choice! \n" + "Who is the next one?");
-  } else if (answer > 11 || answer < 0) {
+  } else if (answer > 11 || answer < 0 ) {
     alert("Please choose right number");
   } else {
-    alert("Please write a number");
+    alert("Please write the number\n\nIf you want to exit please press Exit");
   }
 }
